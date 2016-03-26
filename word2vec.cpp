8b448fb29ed8cb9ae6c0f1062a1d08d98268eac3
senochow <eeseno.chow@gmail.com>
2016-03-26 11:46:32 +0800

    add

---
+++ word2vec.cpp
@@ -0,0 +1,14 @@
+/* ############################################################################
+* 
+* Copyright (c) 2016 ICT MCG Group, Inc. All Rights Reserved
+* 
+* ###########################################################################
+* Brief: 
+ 
+* Authors: zhouxing(@ict.ac.cn)
+* Date:    2016/03/26 11:43:30
+* File:    word2vec.cpp
+*/
+#include <iostream>
+using namespace std;
+
---
+++ word2vec.h
@@ -0,0 +1,14 @@
+/* ############################################################################
+* 
+* Copyright (c) 2016 ICT MCG Group, Inc. All Rights Reserved
+* 
+* ###########################################################################
+* Brief: 
+ 
+* Authors: zhouxing(@ict.ac.cn)
+* Date:    2016/03/26 11:45:17
+* File:    word2vec.h
+*/
+#include <iostream>
+using namespace std;
+
