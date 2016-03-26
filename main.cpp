5371a9f1c041ba5ac72751c334673aa1a134b918
senochow <eeseno.chow@gmail.com>
2016-03-26 11:10:12 +0800

    add

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* ############################################################################
+* 
+* Copyright (c) 2016 ICT MCG Group, Inc. All Rights Reserved
+* 
+* ###########################################################################
+* Brief: 
+ 
+* Authors: zhouxing(@ict.ac.cn)
+* Date:    2016/03/26 11:08:28
+* File:    main.cpp
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+}
